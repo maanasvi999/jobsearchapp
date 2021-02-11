3f8932df97875dbbb822956701d26c42d8e6d78c
maanasvi999 <maanasvi999@gmail.com>
2021-02-11 18:42:50 +0530

    Debug

--- JobSearch/settings.py
+++ JobSearch/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = "FALSE"
 
 ALLOWED_HOSTS = ['127.0.0.1', 'bluecollarjobs.herokuapp.com']
 
@@ -119,6 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
