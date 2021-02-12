67196a42eb211faba2368941c7efa1d64f19b016
maanasvi999 <maanasvi999@gmail.com>
2021-02-13 03:18:15 +0530

    Settings changes

--- JobSearch/settings.py
+++ JobSearch/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "FALSE"
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'bluecollarjobs.herokuapp.com']
 
