6b5a65823d34e8186f7c6d9f7c89ae31f2871bf9
maanasvi999 <maanasvi999@gmail.com>
2021-02-12 21:47:22 +0530

    changes

--- JobSearch/settings.py
+++ JobSearch/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
 django_heroku.settings(locals())
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
